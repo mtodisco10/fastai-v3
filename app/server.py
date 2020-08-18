20b1561720f43ec302cf242f4d02a824e33d0a33
Michael Todisco <mtodisco10@gmail.com>
2020-08-17 20:54:45 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1e3oTF8TaudAMkSVJ356nmvxcJxBhbjrA'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1bEnzqvaSp19mA3-_Q6vGKH8jiQlc8t50'
 export_file_name = 'export.pkl'
 
 classes = ['classical', 'gothic', 'modern', 'victorian']
