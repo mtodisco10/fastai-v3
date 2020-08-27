2f4c2cb9c7ad3d58d031e07865ff839453faf870
Michael Todisco <mtodisco10@gmail.com>
2020-08-26 21:37:48 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,10 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    confidence = learn.predict(img)[1]
+    return JSONResponse({'result': str(prediction),
+                         'confidence': str(confidence)
+                        })
 
 
 if __name__ == '__main__':
